41896fe7e823d6e6bfb972ec936594d1dde5fbad
Nick <nickshulhin@192-168-1-122.tpgi.com.au>
2022-03-08 23:27:52 +1100

    Added case not found scenario

--- src/pages/Case.tsx
+++ src/pages/Case.tsx
@@ -7,6 +7,7 @@ import {FileMetadata, getLinkedToCaseFiles} from "../reducers/fileReducer";
 import {useNavigate, useParams} from "react-router-dom";
 import {Document, Page} from 'react-pdf';
 import {pdfjs} from 'react-pdf';
+import {ResultComponent} from "../components/ResultComponent";
 
 pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/legacy/build/pdf.worker.min.js`;
 
@@ -33,38 +34,47 @@ export function Case() {
 
 
     return (
-        <div style={{ height: '100%'}}>
+        <div style={{height: '100%'}}>
             <Spin spinning={cases.inProgress || files.inProgress}>
-                <Card>
-                    <Descriptions column={1} title="Case info">
-                        <Descriptions.Item label="Title">{cases.viewingCase.title}</Descriptions.Item>
-                        <Descriptions.Item label="Description">{cases.viewingCase.description}</Descriptions.Item>
-                        <Descriptions.Item label="Reported by">
-                            <Tag color="#87d068">{cases.viewingCase.reportedByName}</Tag>
-                        </Descriptions.Item>
-                        <Descriptions.Item
-                            label="Tags">{cases.viewingCase.tags ? cases.viewingCase.tags.map((tag, index) => <Tag
-                            key={index}>{tag}</Tag>) : <div/>}</Descriptions.Item>
-                    </Descriptions>
-                    <div style={{
-                        display: 'flex',
-                        justifyContent: 'space-evenly',
-                        backgroundColor: 'grey',
-                        flexDirection: 'row',
-                        overflowX: 'scroll'
-                    }}>
-                        {
-                            files.linkedToCaseFiles.map((fileMetadata: FileMetadata) =>
-                                <div style={{ display: 'flex', justifyContent: 'center', flexDirection: 'column', padding: 10}}>
-                                    <Button type="primary" key="3" onClick={() => openAttachment(fileMetadata.mediaLink)}>View attachment</Button>
-                                    <Document file={fileMetadata.mediaLink}>
-                                        <Page height={800} pageNumber={1} renderAnnotationLayer={false}/>
-                                    </Document>
-                                </div>
-                            )
-                        }
-                    </div>
-                </Card>
+                {
+                    cases.viewingCase ? <Card>
+                        <Descriptions column={1} title="Case info">
+                            <Descriptions.Item label="Title">{cases.viewingCase.title}</Descriptions.Item>
+                            <Descriptions.Item label="Description">{cases.viewingCase.description}</Descriptions.Item>
+                            <Descriptions.Item label="Reported by">
+                                <Tag color="#87d068">{cases.viewingCase.reportedByName}</Tag>
+                            </Descriptions.Item>
+                            <Descriptions.Item
+                                label="Tags">{cases.viewingCase.tags ? cases.viewingCase.tags.map((tag, index) => <Tag
+                                key={index}>{tag}</Tag>) : <div/>}</Descriptions.Item>
+                        </Descriptions>
+                        <div style={{
+                            display: 'flex',
+                            justifyContent: 'space-evenly',
+                            backgroundColor: 'grey',
+                            flexDirection: 'row',
+                            overflowX: 'scroll'
+                        }}>
+                            {
+                                files.linkedToCaseFiles.map((fileMetadata: FileMetadata) =>
+                                    <div style={{
+                                        display: 'flex',
+                                        justifyContent: 'center',
+                                        flexDirection: 'column',
+                                        padding: 10
+                                    }}>
+                                        <Button type="primary" key="3"
+                                                onClick={() => openAttachment(fileMetadata.mediaLink)}>View
+                                            attachment</Button>
+                                        <Document file={fileMetadata.mediaLink}>
+                                            <Page height={800} pageNumber={1} renderAnnotationLayer={false}/>
+                                        </Document>
+                                    </div>
+                                )
+                            }
+                        </div>
+                    </Card> : <ResultComponent status="error" title="Case not found!" subTitle="It could be removed or just a wrong ID... Life is full of mysteries."/>
+                }
             </Spin>
         </div>
     );
