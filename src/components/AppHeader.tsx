8d9e8aaf1db4f4d8ea67bed3cc128bfec8e171d3
Nick <nickshulhin@192-168-1-122.tpgi.com.au>
2022-03-09 23:07:43 +1100

    Added realtime sockets

--- src/components/AppHeader.tsx
+++ src/components/AppHeader.tsx
@@ -1,14 +1,37 @@
-import {Button} from "antd";
+import {Badge, Button} from "antd";
 import {useNavigate} from "react-router-dom";
 import {useSelector} from 'react-redux'
 import {RootState} from "../store/store";
 import {Header} from "antd/es/layout/layout";
-import React from "react";
+import React, {useEffect, useState} from "react";
+import io, {Socket} from 'socket.io-client';
+import {getAPIUrl} from "../utils/apiUtils";
 
 
 export function AppHeader() {
     const navigate = useNavigate();
     const auth = useSelector((state: RootState) => state.loginReducer)
+    const [userCountOnline, setUserCountOnline] = useState(0)
+    const [socket, setSocket] = useState<Socket>()
+
+    useEffect((): any => {
+        const socket = io(getAPIUrl());
+        setSocket(socket)
+        return () => socket.close();
+    }, []);
+
+    useEffect((): any => {
+        if (socket) {
+            socket.on('userCount', (userCount: number) => {
+                console.log('set user count!')
+                setUserCountOnline(userCount)
+            });
+            socket.on('disconnect', () => {
+                console.log('disconnected :(')
+            });
+        }
+    }, [socket]);
+
 
     function goToLogin() {
         navigate("/login");
@@ -21,7 +44,8 @@ export function AppHeader() {
     return (
 
         <Header style={{padding: 0, display: 'flex', justifyContent: 'flex-end'}}>
-            <div style={{ paddingRight: 30}}>
+            <div style={{paddingRight: 30}}>
+                <Badge color="green" style={{ color: 'white', paddingRight: 30}} text={`Users online: ${userCountOnline}`} />
                 {
                     !auth.isLoggedIn ? <Button type="primary" key="3" onClick={goToLogin}>Login</Button> :
                         <Button type="primary" key="1" onClick={goToAccount}>Hey, {auth.username}!</Button>
